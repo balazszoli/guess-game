da164a53f70d1662554c79fdc09bc2370fadc7df
Zoltan Balazh <48809919+balazszoli@users.noreply.github.com>
2020-06-01 14:43:06 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+secret_word = 12
+guess = 0
+
+while int(guess) != secret_word:
+    guess = int(input("Találd ki a titkos egész számot: "))
+    if guess > secret_word:
+        print("Kisebb szám kell!")
+    elif guess < secret_word:
+        print("Nagyobb szám kell!")
+
+print("Talált!!!")
